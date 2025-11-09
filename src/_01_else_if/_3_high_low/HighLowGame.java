77424fd7e909f09108f5b68c84778db3dc9c92d8
lawsonwangsdgmail.com <lawsonwangsdgmail.com@10.0.0.75>
2025-11-08 22:54:41 -0800

    Done with WalkOfFame

--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -12,7 +12,7 @@ public class HighLowGame {
 		int random = new Random().nextInt(100)+1;
 		
 		// 2. Print out the random variable above
-		System.out.println(random);
+		//    System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,11 +9,20 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-		rob.setX(100);
+		rob.setX(25);
+		rob.setSpeed(100);
+		int x = rob.getX();
 		// 2. Make the robot draw a star shape. Hint: angle=144.
 		rob.penDown();
 		rob.turn(36);
-		
+		while (x < 850) {
+		for (int a = 0; a < 5; a ++) {
+			rob.move(50);
+			rob.turn(144);
+			}
+		x = x + 90;
+		rob.setX(x);
+		}
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
